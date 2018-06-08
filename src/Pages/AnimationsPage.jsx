a40361a98f987d203b109081e01080c93223b39d
Paul Bayruns <bayrunsp9@students.rowan.edu>
2018-06-07 23:42:35 -0400

    Add animations page and react player

--- src/Pages/AnimationsPage.jsx
+++ src/Pages/AnimationsPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { PAGES } from '../Routing/constants.js';
 import RaisedButton from 'material-ui/RaisedButton';
+import YouTubePlayer from 'react-player/lib/players/YouTube'
+
 
 import myHead from "../img/my head.svg";
 import Sitebar from '../Sitebar';
@@ -10,37 +12,34 @@ const buttonStyle = {
     margin: "12px"
 }
 
+const style = {
+    marginTop: "75px",
+};
 
 export default class AnimationsPage extends React.Component {
 
+    constructor(props) {
+        this.state = {
+            url: 'https://www.youtube.com/watch?v=X3EWpvfs8jc'
+        };
+    }
+    
     render() {
         return (
             <React.Fragment>
                 <Sitebar />
+                <div style={style} className="container">
+                    <div className="row justify-content-center">
+                        <h1>Animations</h1>
+                    </div>
+                </div>
                 <div className="container container-padded">
                     <div className="row justify-content-center">
-                        <div className="img-wrapper-wrapper">
-                            <div className="img-center-wrapper">
-                                <img className="img-circle" src={myHead} alt="A vector graphic of my head" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row justify-content-center">
-                        <p className="body-copy">Hey there, I'm Paul. I'm currently a full stack developer for <a href="http://fitdegree.com/">fitDEGREE</a>,
-                working primarily in Angular 2 and PHP (Silex). I'm also experienced with React, Django, Java, C#, SQL, and Amazon Web Services.</p>
-                    </div>
-                    <div className="row  justify-content-center">
-                        <p className="body-copy">I like to engage both halves of my brain, having split my free time at college
-                        between the Computer Science and Art departments. As a result, I've picked up a variety of interests over the years including photography, video art, animation, game design, and ASL. </p>
-                    </div>
-                    <div className="row  justify-content-center">
-                        <p className="body-copy">A lot of great ideas start small, so I think it's important to support learning with development of personal projects.
-                    </p>
-                    </div>
-                    <div className="row justify-content-center">
-                        <Link to={PAGES.PROJECTS.URL}>
-                            <RaisedButton label="See Projects" primary style={buttonStyle} />
-                        </Link>
+                        <YouTubePlayer
+                            url={this.state.url}
+                            playing
+                            controls
+                        />
                     </div>
                 </div>
             </React.Fragment>
